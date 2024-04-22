21a97c24234b1ef5072d10e55f5599986d0bae52
Kaya Wolfert <kayawolfert@web.de>
2024-04-22 09:48:23 +0200

    complete first days task

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,28 @@
+import ColorCard from "./components/ColorCard.js";
 import { themes } from "./db.js";
+import "./App.css";
 
 function App() {
+
+  const theme = themes[0];
   return (
- <div> Inhalt kommt </div>
-  );
+    <>
+ <header> <h1>Theme Creator </h1></header>
+ <main>
+  <section> 
+  <h2> {theme.name}</h2>
+  <> {theme.colors.map ((color) => { 
+    return (
+    <div key={color.value}> 
+    <ColorCard color={color} />
+    </div> )
+})}
+  
+  </>
+  
+  </section>
+ </main>
+ </> );
 }
 
 export default App;

aff80c75bfc9ff53c83828746c6723a7e90a94cd
Kaya Wolfert <kayawolfert@web.de>
2024-04-24 10:41:45 +0200

    add task4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,35 +6,39 @@ import { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function App() {
-
   const [themes, setThemes] = useState(initialThemes);
 
   function handleAddTheme(newTheme) {
     setThemes([{ ...newTheme, id: uuid() }, ...themes]);
   }
 
-
+  function handleDeleteTheme(id) {
+    console.log("you clicked delete" + id);
+    setThemes(themes.filter((theme) => theme.id !== id));
+  }
 
   return (
     <>
- <header> <h1>Theme Creator </h1></header>
- <main>
- <ThemeForm onSubmit={handleAddTheme}/>
-  
-    {themes.map((theme)=> {
-      return (
-        
-        <Theme key={theme.id} theme={theme} name={theme.name}/>
-        
-        
-      )
-    })}
+      <header>
+        {" "}
+        <h1>Theme Creator </h1>
+      </header>
+      <main>
+        <ThemeForm onSubmit={handleAddTheme} />
 
-  
-  
- </main>
- </> );
+        {themes.map((theme) => {
+          return (
+            <Theme
+              key={theme.id}
+              theme={theme}
+              name={theme.name}
+              onDelete={() => handleDeleteTheme(theme.id)}
+            />
+          );
+        })}
+      </main>
+    </>
+  );
 }
 
 export default App;
-
